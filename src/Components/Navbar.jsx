3e8358d41c2157b5146e8457b04bd78fc3a996bb
Fernando castro <fernandocastro77@yahoo.com>
2023-04-06 15:00:34 -0400

    just finished on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useEffect, useState } from "react";
@@ -8,9 +10,10 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddindx} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -22,15 +25,65 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-17 h-14 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Fernando Castro<span className="sm:block hidden"> | Full Stack Deveolper</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+            Fernando Castro &nbsp;
+            <span className="sm:block hidden"> | Full Stack Deveolper</span>
           </p>
         </Link>
-        <p className="text-red-500"> dddcm</p>
+        <ul
+          className="list-none hidden sm:flex
+        flex-row gap-10"
+        >
+          {navLinks.map((link) => (
+            // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px]
+            font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}> {link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-28[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px]
+          z-10 rounded-x1`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  }font-poppins font-medium cursor-pointer text-[16px] `}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}> {link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-

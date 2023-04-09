8094bf22026a2471f9b69602363bd7a6b77c8ff0
Fernando castro <fernandocastro77@yahoo.com>
2023-04-09 18:08:55 -0400

    just starting on feedback section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,58 +4,61 @@ import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
-import { fadeIn , textVariant } from "../utils/motion";
+import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
 
-const ServiceCard = ({index, title, icon }) => {
-    return (
-        <Tilt className="xs:w-[250px] w-full">
-            <motion.div 
-            variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-            className="w-full green-pink-grandient p-[1px] rounded-[20px] shadow-card">
-                <div options={{
-                    max: 45,
-                    scale: 1,
-                    speed: 450
-                }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
-
-                    <img src={icon} alt={title}
-                    className="w-16 h-16 object-contain" />
-                    <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
-
-                </div>
-
-            </motion.div>
-         </Tilt>
-
-    )
-
-}
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-grandient p-[1px] rounded-[20px] shadow-card"
+        style={{ willChange: "transform, opacity" }}
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 300,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-    <p className={styles.sectionSubText}>Introduction</p>
-    <h2 className={styles.sectionHeadText}>Overview.</h2>
-    </motion.div>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-    <motion.p 
-    variants={fadeIn("","",0.1,1)}
-    className="mt-4 text-secondary text -[17px] max-w-3x1 leading-[30px]">
-        im a software engineer with a passion for creating beautiful and functional user interfaces.
-        i have a strong background in 3d graphics and animation, which i use to create unique and memorable user experiences.
-         i have a strong background in 3d graphics and animation, which i use to create unique and memorable user experiences.
-    </motion.p>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text -[17px] max-w-3x1 leading-[30px]"
+      >
+        im a software engineer with a passion for creating beautiful and
+        functional user interfaces. i have a strong background in 3d graphics
+        and animation, which i use to create unique and memorable user
+        experiences. i have a strong background in 3d graphics and animation,
+        which i use to create unique and memorable user experiences.
+      </motion.p>
 
-    <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-10 justify-center items-center">
         {services.map((service, index) => (
-            <ServiceCard key={service.title} index={index} {...service} />
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
-    </div>
+      </div>
     </>
-
-  )
+  );
 };
 
 export default SectionWrapper(About, "about");
--- src/Components/Feedbacks.jsx
+++ src/Components/Feedbacks.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
 
 const Feedbacks = () => {
-  return <div>Feedbacks</div>;
+  return <div className="mt-12 bg-black-100 rounded-[20px]">
+
+  </div>;
 };
 
 export default Feedbacks;

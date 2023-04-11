355013a858e728efe8db0b4b3bc9ba26bf53b8a9
Fernando castro <fernandocastro77@yahoo.com>
2023-04-11 13:44:29 -0400

    adding the earth 3D model

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,30 +5,25 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
-        <div className="flex flex-col">
-          <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>
-              Hi, Im <span className="text-[#915EFF]">Adrian</span>
-            </h1>
-            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop 3D visuals, user <br className="sm:block hidden" />
-              interfaces and web applications
-            </p>
-          </div>
-          <div className="mt-10 flex justify-center">
-            <div className="w-full sm:w-1/2">
-              <ComputersCanvas />
-            </div>
-          </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, Im <span className="text-[#915EFF]">Adrian</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className="sm:block hidden" />
+            interfaces and web applications
+          </p>
         </div>
       </div>
+
+      <ComputersCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
@@ -42,7 +37,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-
-
-
